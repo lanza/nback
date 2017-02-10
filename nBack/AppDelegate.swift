c3d5b031db0a179979b7de6c0249d9c53f6b9818
Nathan Lanza <nathan@lanza.io>
2017-02-10 02:50:40 -0500

    Release 1.1.2b1

--- nBack/AppDelegate.swift
+++ nBack/AppDelegate.swift
@@ -10,8 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        appCoordinator.start()
-        window?.rootViewController = appCoordinator.viewController
+        window?.rootCoordinator = appCoordinator
         window?.makeKeyAndVisible()
         
         return true
