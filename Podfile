1b19e40123817b3ecd3967483653e9fc22da9097
Nathan Lanza <nathan@lanza.io>
2017-03-11 01:09:01 -0500

    Working on JSONParser

--- Podfile
+++ Podfile
@@ -1,25 +1,33 @@
 # vim : set ft=ruby :
 
-target 'nBack' do
-  use_frameworks!
+def podsForTarget(target)
 
-  
-  if `whoami`[0...-1] == 'lanza'
-    pod 'CoordinatorKit', :path => '~/Documents/CoordinatorKit'
-    pod 'Reuse', :path => '~/Documents/Reuse'
-  else 
-    pod 'CoordinatorKit', :git => 'https://github.com/nathanlanza/CoordinatorKit'
-    pod 'Reuse', :git=> 'https://github.com/nathanlanza/Reuse'
+  target target do
+    use_frameworks!
+
+    pod 'SwiftyJSON'
+    
+    if `whoami`[0...-1] == 'lanza'
+      pod 'CoordinatorKit', :path => '~/Documents/CoordinatorKit'
+      pod 'Reuse', :path => '~/Documents/Reuse'
+    else 
+      pod 'CoordinatorKit', :git => 'https://github.com/nathanlanza/CoordinatorKit'
+      pod 'Reuse', :git=> 'https://github.com/nathanlanza/Reuse'
+    end
+
+    pod 'RxSwift', '~> 3.2'
+    pod 'RxCocoa', '~> 3.2'
+    pod 'RxDataSources'
+    pod 'RealmSwift'
+    pod 'RxRealm'
+
+    pod 'DZNEmptyDataSet'
+    pod 'BonMot'
+    pod 'Eureka', '~> 2.0.0-beta.1'
+
   end
-
-  pod 'RxSwift', '~> 3.2'
-  pod 'RxCocoa', '~> 3.2'
-  pod 'RxDataSources'
-  pod 'RealmSwift'
-  pod 'RxRealm'
-
-  pod 'DZNEmptyDataSet'
-  pod 'BonMot'
-  pod 'Eureka', '~> 2.0.0-beta.1'
-
 end
+
+podsForTarget 'nBack'
+podsForTarget 'nBackInternal'
+podsForTarget 'nBackTests'
--- nBack/JSONParser.swift
+++ nBack/JSONParser.swift
@@ -1,2 +1,73 @@
 import Foundation
+import SwiftyJSON
 
+class JSONParser {
+    
+    static func parse() {
+        
+        let url = Bundle.main.url(forResource: "oldData", withExtension: "json")
+        let data = try! Data(contentsOf: url!)
+        
+        let anyJSON = try! JSONSerialization.jsonObject(with: data, options: .allowFragments)
+        let json = JSON(anyJSON)
+       
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd HH:mm:ss +0000"
+        
+        print(json.arrayValue.count)
+        
+        for gameResultDict in json.array! {
+            let numberOfTurns = gameResultDict["numberOfTurns"].int!
+            let level = gameResultDict["level"].int!
+            let timeBetweenTurns = gameResultDict["timeBetweenTurns"].int!
+            let dateString = gameResultDict["date"].string!
+            print(dateString)
+            let date = df.date(from: dateString)!
+            print(date)
+            
+            let gr = GameResultRealm.new(columns: 3, rows: 3, level: level, numberOfTurns: numberOfTurns, secondsBetweenTurns: Double(timeBetweenTurns), squareHighlightTime: 0.5)
+            try! gr.realm?.write {
+                gr.date = date
+            }
+           
+            let typeResults = gameResultDict["types"].array!
+            for typeResultDict in typeResults {
+                
+                let correct = typeResultDict["correct"].int!
+                let incorrect = typeResultDict["incorrect"].int!
+                let matches = typeResultDict["matches"].int!
+                let typeInt = typeResultDict["type"].int!
+                let type: NBackType = typeInt == 1 ? .squares : .numbers
+                
+                let tr = TypeResultRealm.new(c: correct, i: incorrect, m: matches, nBackType: type)
+                gr.add(typeResult: tr)
+            }
+            
+        }
+        
+    }
+}
+
+import RealmSwift
+fileprivate extension TypeResultRealm {
+    
+    static func new(c: Int, i: Int, m: Int, nBackType: NBackType) -> TypeResultRealm {
+    
+    let realm = try! Realm()
+    
+    let t = TypeResultRealm()
+    
+    try! realm.write {
+      
+      t.correct = c
+      t.incorrect = i
+      
+      t.matches = m
+      
+      t.nBackType = nBackType
+      
+      realm.add(t)
+    }
+    return t
+  }
+}
