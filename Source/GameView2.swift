e30285001dc3ce6d3075d7793715c972bdeb54b0
Nathan Lanza <nathan@lanza.io>
2020-01-25 15:55:52 -0800

    Add the test version of the SwiftUI based GameView

---
+++ Source/GameView2.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  nBack
+//
+//  Created by Nathan Lanza on 1/25/20.
+//
+
+import SwiftUI
+
+struct GameView2: View {
+    var body: some View {
+        Text("Hi")
+    }
+}
+
+struct GameView2_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView2()
+    }
+}
